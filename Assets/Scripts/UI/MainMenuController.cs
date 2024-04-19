d6847df995dba40bdf77cf311f86ca7d3a6ddfce
Florencia Pak <100231396+florpak@users.noreply.github.com>
2024-04-19 18:46:45 -0300

    Coding Main Menu Script

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -5,7 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject mainPanel;
+    [SerializeField] GameObject instructionsPanel;
+    [SerializeField] GameObject creditsPanel;
+
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
     void Start()
     {
         
@@ -15,5 +25,49 @@ public class MainMenuController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void Play(string sceneName)
+    {
+        
+        // TODO: Unload all data from previous sessions, then start clean
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+
+    public void Instructions()
+    {
+        mainPanel.SetActive(false);
+        instructionsPanel.SetActive(true);
+    }
+
+    public void GoToGoal()
+    {
+        mainPanel.SetActive(false);
+        instructionsPanel.SetActive(false);
+    }
+
+    public void Credits()
+    {
+        mainPanel.SetActive(false);
+        creditsPanel.SetActive(true);
+    }
+
+    public void BackToMainMenu()
+    {
+        mainPanel.SetActive(true);
+        instructionsPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    private void PlaySound(AudioClip audioClip)
+    {
+        _audioSource.clip = audioClip;
+        _audioSource.Play();
     }
 }
