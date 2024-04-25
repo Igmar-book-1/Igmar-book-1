aee048696b25d7953c699f172f3d2734f23c9222
Agustin Piper <piper_liuterman@outlook.com>
2024-04-25 14:30:50 -0300

    Enemy Patrol Base

---
+++ Assets/Scripts/Enemy Agus/States/State.cs
@@ -0,0 +1,39 @@
+﻿using LineOfSight;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace States
+{
+    public abstract class State : MonoBehaviour
+    {
+        protected NavMeshAgent Agent;
+        private LineOfSightBehaviour _lineOfSight;
+        public float lineOfSightRange;
+        public float lineOfSightAngle;
+        public float speed;
+
+        private void Awake()
+        {
+            Agent = GetComponent<NavMeshAgent>();
+            _lineOfSight = GetComponent<LineOfSightBehaviour>();
+        }
+
+        public virtual void Execute()
+        {
+            SetupLineOfSight();
+            SetupSpeed();
+        }
+
+        private void SetupSpeed()
+        {
+            Agent.speed = speed;
+        }
+
+        private void SetupLineOfSight()
+        {
+           _lineOfSight.range = lineOfSightRange;
+           _lineOfSight.angle = lineOfSightAngle;
+        }
+        public LineOfSightBehaviour GetLineOfSight() => _lineOfSight;
+    }
+}
