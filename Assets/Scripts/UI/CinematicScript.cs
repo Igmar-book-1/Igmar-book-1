1c205d04a8052c535cacccb2c614266df8a78f9b
Florencia Pak <100231396+florpak@users.noreply.github.com>
2024-06-23 20:45:28 -0300

    Totem modificado y Script de Cinameticas modificado

--- Assets/Scripts/UI/CinematicScript.cs
+++ Assets/Scripts/UI/CinematicScript.cs
@@ -8,16 +8,25 @@ public class CinematicScript : MonoBehaviour
 {
     private VideoPlayer _videoPlayer;
     public string NextSceneName;
-
+    AsyncOperation _asynOperation;
     void Awake()
     {
         _videoPlayer = GetComponent<VideoPlayer>();
         _videoPlayer.Play();
         _videoPlayer.loopPointReached += CheckOver;
     }
+    void Start()
+    {
+
+    }
 
     private void Update()
     {
+        if(this._asynOperation == null)
+        {
+            this.StartCoroutine(LoadSceneAsync(NextSceneName));
+        }
+
         if (Input.anyKeyDown)
         {
             CheckOver(_videoPlayer);
@@ -26,6 +35,22 @@ public class CinematicScript : MonoBehaviour
 
     public void CheckOver(VideoPlayer vp)
     {
-        SceneManager.LoadScene(NextSceneName, LoadSceneMode.Single);
+        if(this._asynOperation != null)
+        {
+            this._asynOperation.allowSceneActivation = true;
+        }
+        //SceneManager.LoadScene(NextSceneName, LoadSceneMode.Single);
     }
+
+    private IEnumerator LoadSceneAsync(string nameScene)
+    {
+        this._asynOperation = SceneManager.LoadSceneAsync(nameScene);
+        this._asynOperation.allowSceneActivation = false;
+        while (!this._asynOperation.isDone)
+        {
+            Debug.Log("La escena cargó " + this._asynOperation.progress + "%");
+            yield return null;
+        }
+    }
+
 }
